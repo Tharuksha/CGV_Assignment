238e6a5870e3659dd463042651a10aeb008ff33f
Jaliya Madusanka <87718010+Jaliya98@users.noreply.github.com>
2024-09-09 00:02:11 +0530

    save the image

--- [2]threshold_image.py
+++ [2]threshold_image.py
@@ -4,3 +4,9 @@ import os
 def get_image_path(filename):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(script_dir, filename)
+
+def save_image(image, subfolder, filename):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(script_dir, subfolder)
+    os.makedirs(path, exist_ok=True)
+    cv2.imwrite(os.path.join(path, filename), image)
