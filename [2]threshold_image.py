8fc3c2055313f111439c5bc8b1c4eeeae8091f5d
Jaliya Madusanka <87718010+Jaliya98@users.noreply.github.com>
2024-09-09 00:02:56 +0530

    create the adaptive binary image

--- [2]threshold_image.py
+++ [2]threshold_image.py
@@ -10,3 +10,9 @@ def save_image(image, subfolder, filename):
     path = os.path.join(script_dir, subfolder)
     os.makedirs(path, exist_ok=True)
     cv2.imwrite(os.path.join(path, filename), image)
+
+def adaptive_binarize_image(image):
+    # Apply adaptive thresholding
+    binary_image = cv2.adaptiveThreshold(
+        image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    return binary_image
