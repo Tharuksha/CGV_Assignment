b87a7c3499abec959683fd3417b4a62f10aa2903
ravindu manawa <ravindumanawa7@gmail.com>
2024-09-09 16:00:01 +0530

    Setup basic repository structure with file path function

--- [6]visualize_sales.py
+++ [6]visualize_sales.py
@@ -1,1 +1,7 @@
-#preview
+import matplotlib.pyplot as plt
+import os
+import ast
+
+def get_file_path(filename):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(script_dir, filename)
